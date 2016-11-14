f78a7a747b15cd1369fa51028cf57aad8ffd8170
Evgenia Gurova <izhenka@gmail.com>
2016-11-15 00:28:46 +0100

    read message

--- Hjemmeeksamen/server.c
+++ Hjemmeeksamen/server.c
@@ -1,6 +1,6 @@
+#include <stdio.h>
 
-
-
+int read_next_job(FILE* file, char* work_type, unsigned char* message_len, char* message);
 
 /*
 * Server!
@@ -19,16 +19,40 @@ int main(int argc, char const *argv[]) {
   }
 
   //if client connected
-  int str_length = 0;
-  int bytes_read = fread(&str_length, sizeof(int), 1, file);
-  if (bytes_read < 1){
-    printf("Couldn't read next byte from file\n");
-    return -1; //TODO send message to klient?
-  }
-  printf("str_length: %d\n", str_length);
+  char work_type;
+  unsigned char message_len;
+  char message[256] = {0};
 
+  if (read_next_job(file, &work_type, &message_len, message) == -1){
+    //TODO send message to klient?
+  }
 
 
   fclose(file);
   return 0;
+}
+
+/* Reads next job parameters from file into input parameters
+*  returns -1 on error (f.eks end of file is reached), else 0
+*/
+int read_next_job(FILE* file, char* work_type, unsigned char* message_len, char* message){
+
+  int bytes_read = 0;
+
+  fread(work_type, sizeof(char), 1, file);
+  printf("work_type: %c\n", *work_type);
+
+  fread(message_len, sizeof(unsigned char), 1, file);
+  printf("message_len: %u\n", *message_len);
+
+  bytes_read = fread(message, sizeof(char), *message_len, file);
+  printf("message: %s\n", message);
+
+  if (bytes_read < 1){
+    printf("Couldn't read next byte from file\n");
+    return -1;
+  }
+
+  return 0;
+
 }
