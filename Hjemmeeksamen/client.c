0dd4db81d32a339bd80c26e090c916529d91b7de
Evgenia Gurova <izhenka@gmail.com>
2016-11-16 18:44:45 +0100

    server-klient funker som faen!

--- Hjemmeeksamen/client.c
+++ Hjemmeeksamen/client.c
@@ -11,10 +11,28 @@
 #include <signal.h>
 #include <errno.h>
 
+void get_job(int* exit);
+void get_x_jobs(int* exit);
+void get_all_jobs(int* exit);
+void terminate(int signum);
 int connect_to_server(char* ip, int port);
+void commando_loop(int* termination_type);
+int receive_from_serv(char* msg, char* work_type);
+int send_req(char c, int num);
+int send_exit();
+int send_error_exit();
+int send_exit_after_req();
 
-int main(int argc, char *argv[])
-{
+//GLOBAL VARIABLES
+int g_sock; //client socket
+int user_terminate = 0;
+
+#define INCOM_MSG_SIZE 258
+#define REQ_MSG_SIZE 2
+
+
+//---*****------MAIN -----------****----------
+int main(int argc, char *argv[]){
 	if (argc != 3) {
 		fprintf(stderr, "Usage: %s <ip> <port>\n", argv[0]);
 		return -1;
@@ -26,32 +44,38 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	int sock = connect_to_server(argv[1], port);
-	if (sock == -1){
+	//Cntrl + c handler
+	struct sigaction sa;
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = terminate;
+
+	if (sigaction(SIGINT, &sa, NULL) != 0) {
+		perror("sigaction()");
+		exit(EXIT_FAILURE);
+	}
+
+
+	g_sock = connect_to_server(argv[1], port);
+	if (g_sock == -1){
     exit(EXIT_FAILURE);
 	}
 
-	char msg[256] = "Hei, server!";
-	ssize_t ret = send(sock, msg, strlen(msg), 0);
-	if (ret == -1) {
-		perror("send()");
-		close(sock);
-		exit(EXIT_FAILURE);
+	int termination_type = 0;
+	commando_loop(&termination_type);
+
+	int conf_sent = 0;
+	switch (termination_type) {
+		case 1: 	conf_sent = send_exit(); break;
+		case 2: 	conf_sent = send_exit_after_req();
+							printf("Server is done with jobs and asked to terminate.\n");
+		 					break;
+		case -1: 	conf_sent = send_error_exit(); break;
+		default: 	conf_sent = send_error_exit();
 	}
 
-	char buf[256] = { 0 };
-	ret = recv(sock, buf, sizeof(buf) - 1, 0);
-	if (ret == 0) {
-		printf("Server disconnected.\n");
-	} else if (ret == -1) {
-		perror("recv()");
-		close(sock);
-		exit(EXIT_FAILURE);
-	} else {
-		printf("Message from server: %s\n", buf);
-	}
+	printf("Confirmation of termination %s\n", conf_sent ? "has been sent successfully!" : "hasn't been sent.");
 
-	close(sock);
+	close(g_sock);
 	return 0;
 
 	/*
@@ -72,8 +96,6 @@ int main(int argc, char *argv[])
   }
 
 */
-
-
 
 	// int flags = MAP_SHARED | MAP_ANONYMOUS;
 	// char *mem2 = malloc(512);
@@ -111,6 +133,17 @@ int main(int argc, char *argv[])
 	// return 0;
 }
 
+
+//OTHER FUNCTIONS
+
+/* SIGINT handler
+* terminates connections acception
+*/
+void terminate(int signum){
+	printf("\nTerminating client (signal %d).\n", signum);
+	user_terminate = 1;
+}
+
 /* Establishes connection to server
 * returns socket index
 * returns -1 on error
@@ -123,7 +156,7 @@ int connect_to_server(char* ip, int port){
 		perror("socket()");
 		return -1;
 	}
-  printf("Socket is set!\n");
+  //printf("Socket is set!\n");
 
   //setting internet address
   struct sockaddr_in server_addr;
@@ -140,7 +173,7 @@ int connect_to_server(char* ip, int port){
     close(sock);
     return -1;
   }
-  printf("Sockaddr_in is set %u!\n", server_addr.sin_addr.s_addr);
+  //printf("Sockaddr_in is set %u!\n", server_addr.sin_addr.s_addr);
 
 
 	printf("Connecting to %s:%d\n", ip, port);
@@ -152,4 +185,232 @@ int connect_to_server(char* ip, int port){
 	printf("Connected!\n");
 
   return sock;
+}
+
+void commando_loop(int* termination_type){
+
+  int exit = 0;
+  while(!exit){
+    printf("\nTast kommando:\n");
+    printf("\t1. Hent en jobb fra serveren\n");
+    printf("\t2. Hent X antall jobber fra serveren\n");
+    printf("\t3. Hent alle jobber fra serveren\n");
+    printf("\t4. Avslutte programmet\n");
+
+    char res_s[256];
+    fgets(res_s, 256, stdin);
+    int res = (int) strtol(res_s, NULL, 10);
+
+		if (user_terminate){
+			exit = -1;
+			break;
+		}
+
+    switch (res) {
+      case 1: get_job(&exit); break;
+      case 2: get_x_jobs(&exit); break;
+      case 3: get_all_jobs(&exit); break;
+			case 4: send_exit(); exit = 1; break;
+      default: printf("Ugyldig kommado!\n");
+    }
+
+
+  }
+
+	*termination_type = exit;
+}
+
+/* Gets new job from server
+* 	parameter: int* exit is set to
+* 		-1 when programm should be terminated because of error/server disconnect
+* 		2 on request to terminate from server
+*/
+void get_job(int* exit){
+
+	if (!send_req('G', 0)){
+		printf("Failed to send request to server.\n");
+		return;
+	}
+
+	char msg[256] = { 0 };
+	char work_type;
+	int res = receive_from_serv(msg, &work_type);
+	if (res == -1){
+		*exit = -1;
+		return;
+	} else if (res == 2) {
+		*exit = 2;
+		return;
+	}
+
+	//Got message
+	printf("Message from server: %s\n", msg);
+
+	if (user_terminate){
+		*exit = -1;
+		return;
+	}
+
+
+}
+
+/* Gets X new jobs from server
+* 	parameter: int* exit is set to
+* 		-1 when programm should be terminated because of error/server disconnect
+* 		2 on request to terminate from server
+*/
+void get_x_jobs(int* exit){
+
+	printf("\nAntall jobber [1..255]:");
+  char number_jobs_s[256];
+  fgets(number_jobs_s, 256, stdin);
+  int number_jobs = (int) strtol(number_jobs_s, NULL, 10);
+
+  if (number_jobs<1 || number_jobs>255){
+    fprintf(stdout, "Feil antall\n");
+    return;
+  }
+
+	//sending
+	if (!send_req('N', number_jobs)){
+		printf("Failed to send request to server.\n");
+		return;
+	}
+
+	//receiving
+	char msg[256] = { 0 };
+	char work_type;
+	for (int i = 0; i<number_jobs; i++){
+		int res = receive_from_serv(msg, &work_type);
+		if (res == -1){
+			*exit = -1;
+			return;
+		} else if (res == 2) {
+			*exit = 2;
+			return;
+		}
+		//Got message
+		printf("Message from server: %s\n", msg);
+
+		if (user_terminate){
+			*exit = -1;
+			return;
+		}
+	}
+
+}
+
+/* Gets all jobs left from server
+* 	parameter: int* exit is set to
+* 		-1 when programm should be terminated because of error/server disconnect
+* 		2 on request to terminate from server
+*/
+void get_all_jobs(int* exit){
+	//sending
+	if (!send_req('A', 0)){
+		printf("Failed to send request to server.\n");
+		return;
+	}
+
+	//receiving
+	char msg[256] = { 0 };
+	char work_type;
+	for (;;){
+		//printf("receive_from_serv ...\n");
+		int res = receive_from_serv(msg, &work_type);
+		if (res == -1){
+			*exit = -1;
+			return;
+		} else if (res == 2) {
+			*exit = 2;
+			return;
+		}
+		//Got message
+		printf("Message from server: %s\n", msg);
+
+		if (user_terminate){
+			*exit = -1;
+			return;
+		}
+
+	}
+}
+
+/* Reads next message from server:
+* returns
+	-1 on error,
+	1 job is get
+	2 server asks to terminate
+*/
+int receive_from_serv(char* msg, char* work_type){
+
+	memset(msg, 0, 256);
+	char buf[INCOM_MSG_SIZE] ={0};
+
+	ssize_t ret = recv(g_sock, buf, INCOM_MSG_SIZE, 0);
+	if (ret == 0) {
+		printf("Server disconnected.\n");
+		return -1;
+	} else if (ret == -1) {
+		perror("recv()");
+		return -1;
+	} else {
+
+		// printf("****test*** Message from server:\n");
+		// printf("****test*** buf[0]: %c\n", buf[0]);
+		// printf("****test*** buf[1]: %d\n", buf[1]);
+		// printf("****test*** buf[2]: %s\n", (buf + 2));
+
+		if (buf[0] == 'Q'){
+			return 2; //server has no more jobs
+		}
+
+		*work_type = buf[0];
+		memmove(msg, (buf + 2), strlen(buf)); //already with /0 at the end
+		return 1; //job is get
+	}
+}
+
+/* Sends request to server:
+* Protocol
+* each message of 2 bytes: char and unsigned char
+* 'G'0 	- get 1 job
+* 'A'0 	- get all jobs
+* 'N'x - get x jobs
+* 'T'0 	- client terminates normally
+* 'E'0	- client terminates with error
+*
+* returns 0 on error, 1 on success
+*/
+int send_req(char c, int num){
+	char msg[REQ_MSG_SIZE] = {0};
+	msg[0] = c;
+	msg[1] = (unsigned char) num;
+	ssize_t ret = send(g_sock, msg, REQ_MSG_SIZE, 0);
+	if (ret == -1) {
+		perror("send()");
+		return 0;
+	}
+	return 1;
+}
+
+/* Sends message of normal termination to server
+* returns 0 on error, 1 on success
+*/
+int send_exit(){
+	return send_req('T', 0);
+}
+
+/* Sends message of error termination to server
+* returns 0 on error, 1 on success
+*/
+int send_error_exit(){
+	return send_req('E', 0);
+}
+
+/* Sends message of termination after request from server
+* returns 0 on error, 1 on success
+*/
+int send_exit_after_req(){
+	return send_req('Q', 0);
 }
--- Hjemmeeksamen/server.c
+++ Hjemmeeksamen/server.c
@@ -9,12 +9,19 @@
 #include <signal.h>
 #include <errno.h>
 
-int read_next_job(FILE* file, char* work_type, unsigned char* message_len, char* message);
+char* read_next_job(FILE* file, int* eof_reached);
 int create_socket(int port);
 void terminate(int signum);
 int accept_connection(int server_sock);
+int talk_with_client(FILE* file, int client_sock);
+ssize_t send_next_job(FILE* file, int client_sock, int* eof_reached);
+
+#define INCOM_MSG_SIZE 2
+#define REPLY_MSG_SIZE 258
 
 int running = 1; //continue to accept connections
+int client_running = 0; //continue to exchange with client
+
 
 /*
 * Server!
@@ -54,47 +61,35 @@ int main(int argc, char const *argv[]) {
   if (sock == -1){
     exit(EXIT_FAILURE);
   }
-  printf("Socket created successfully, num: %d\n", sock);
+  //printf("Socket created successfully, num: %d\n", sock);
 
 
   while (running){
+
     //accept connection
-    printf("Waiting for connection...\n");
+    printf("Server: waiting for connections...\n");
     int client_sock = accept_connection(sock);
+    client_running = 1;
+    //printf("New connection accepted. client_sock: %d\n", client_sock);
     if (client_sock == -1){
       continue;
     }
 
-    ssize_t ret;
-    char buf[256] = { 0 };
-  	ret = recv(client_sock, buf, sizeof(buf) - 1, 0);
-    if (ret == 0) {
-    	printf("Client disconnected.\n");
-    }else if (ret == -1) {
-  		perror("recv()");
-  		close(client_sock);
-  		continue;
-  	} else {
-  		printf("Message from client: %s\n", buf);
-  	}
-
-
-    char work_type;
-    unsigned char message_len;
-    char message[256];
-
-    if (read_next_job(file, &work_type, &message_len, message) == -1){
-      //TODO send message to klient?
-      strcpy(message, "I'm tired, bye! :*\n");
-      running = 0;
+    //exchanging with client untill its done
+    while(client_running){
+      //printf("new talk_with_client...\n");
+      int res = talk_with_client(file, client_sock);
+      switch (res) {
+        case 0: printf("Exchange succeed!\n"); break;
+        case 1: printf("Client terminated normally\n"); client_running = 0; break;
+        case 2: printf("Client terminated on error\n"); client_running = 0; break;
+        case 3: printf("Client disconnected\n"); client_running = 0; break;
+        case 5: printf("Client terminated normally after request from server\n");
+                client_running = 0; running = 0; break;
+        default: printf("Exchange failed! Trying again...\n");
+      }
     }
-
-    ret = send(client_sock, message, strlen(message), 0);
-    if (ret == -1){
-      perror("send()");
-      close(client_sock);
-      continue;
-    }
+    //printf("done! talk_with_client...\n");
 
     close(client_sock);
   }
@@ -108,28 +103,46 @@ int main(int argc, char const *argv[]) {
 /* Reads next job parameters from file into input parameters
 *  returns -1 on error (f.eks end of file is reached), else 0
 */
-int read_next_job(FILE* file, char* work_type, unsigned char* message_len, char* message){
+char* read_next_job(FILE* file, int* eof_reached){
+
+  char* res_message;
 
   int bytes_read = 0;
-  printf("****** reading *****\n");
 
-  fread(work_type, sizeof(char), 1, file);
-  printf("work_type: %c\n", *work_type);
+  char work_type;
+  unsigned char message_len = 0;
 
-  fread(message_len, sizeof(unsigned char), 1, file);
-  printf("message_len: %u\n", *message_len);
-
-  memset(message, '\0', 256);
-  bytes_read = fread(message, sizeof(char), *message_len, file);
-  printf("message: %s\n", message);
-
-  if (bytes_read < 1){
-    printf("Couldn't read next byte from file\n");
-    return -1;
+  bytes_read = fread(&work_type, sizeof(char), 1, file);
+  if (bytes_read < 1){ //end of file
+    //quit-message
+    res_message = malloc(3);
+    work_type = 'Q';
+    memmove(res_message, &work_type, 1);
+    memmove(res_message + 1, &message_len, 1);
+    res_message[2] = '\0';
+    *eof_reached = 1;
+    return res_message;
   }
+  //printf("work_type: %c\n", work_type);
 
-  printf("****** reading *****\n");
-  return 0;
+  fread(&message_len, sizeof(unsigned char), 1, file);
+  //printf("message_len: %u\n", message_len);
+
+  char message[256] = {0};
+  fread(message, sizeof(char), message_len, file);
+  //printf("message: %s\n", message);
+
+  res_message = malloc(2 + message_len +1);
+  memmove(res_message, &work_type, 1);
+  memmove(res_message + 1, &message_len, 1);
+  memmove(res_message + 2, message, message_len + 1);
+
+  // printf("res_message: %s, len: %lu\n", res_message, strlen(res_message));
+  // printf("res_message[0]: %c\n", res_message[0]);
+  // printf("res_message[1]: %d\n", res_message[1]);
+  // printf("res_message[2]: %s\n", (res_message + 2));
+
+  return res_message;
 }
 
 /* Creates socket and return its index
@@ -143,7 +156,7 @@ int create_socket(int port){
 		perror("socket()");
 		return -1;
 	}
-  printf("Socket is set!\n");
+  //printf("Socket is set!\n");
 
   //setting internet address
   struct sockaddr_in server_addr;
@@ -151,7 +164,15 @@ int create_socket(int port){
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(port);
   server_addr.sin_addr.s_addr = INADDR_ANY;
-  printf("Sockaddr_in is set %u!\n", server_addr.sin_addr.s_addr);
+  //printf("Sockaddr_in is set %u!\n", server_addr.sin_addr.s_addr);
+
+  //setting reuse of address
+  int yes = 1;
+  if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))) {
+    perror("setsockopt()");
+    close(sock);
+    return -1;
+  }
 
   //binding address to socket
   if (bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0){
@@ -159,15 +180,16 @@ int create_socket(int port){
     close(sock);
     return -1;
   }
-  printf("Address is binded!\n");
+  //printf("Address is binded!\n");
 
   //activate listening to connections
   if (listen(sock, SOMAXCONN) != 0) {
+  //if (listen(sock, 0) != 0) {
 		perror("listen()");
 		close(sock);
 		return -1;
 	}
-  printf("Listening is on!\n");
+  //printf("Listening is on!\n");
 
   return sock;
 
@@ -178,8 +200,9 @@ int create_socket(int port){
 */
 void terminate(int signum)
 {
-	printf("\nTerminating server with signal: %d\n", signum);
+	printf("\nTerminating server (signal %d).\n", signum);
 	running = 0;
+  client_running = 0;
 }
 
 /* Accept client connection
@@ -205,4 +228,105 @@ int accept_connection(int server_sock){
 
   return client_sock;
 
+}
+
+/* Provides exchage with client according to protocol:
+*   receive:
+*     each message of 2 bytes: char and unsigned char
+*     'G'0 	- get 1 job
+*     'A'0 	- get all jobs
+*     'N'x  - get x jobs
+*     'T'0 	- client terminates normally
+*     'E'0	- client terminates with error
+*     'Q'0 	- client terminates normally after request from server. Server can terminate.
+*
+*   send:
+*   ... (se oppgavetekst)
+*   byte 1: job type (O/E/Q)
+*   byte 2: message length
+*   rest:   message
+*
+* returns:
+*    0 exchange succeed
+*    1 client terminated normally
+*    2 client terminated on error
+*    3 client disconnected
+*    4 end of file reached, server should terminate
+*    5 client terminated normally after request from server
+*   -1 on error
+*/
+int talk_with_client(FILE* file, int client_sock){
+
+  //receiving
+  ssize_t ret;
+  char buf[INCOM_MSG_SIZE] = { 0 };
+  //printf("recv from client ...\n");
+  ret = recv(client_sock, buf, INCOM_MSG_SIZE, 0);
+  //printf("reicived from client buf: %s, len buf: %lu, bytes received %zd:\n", buf, strlen(buf), ret);
+  if (ret == 0) {
+    printf("Client disconnected.\n");
+    return 3;
+  } else if (ret == -1) {
+    perror("recv()");
+    return -1;
+  }
+  char req_type = buf[0];
+  unsigned int number_jobs = buf[1];
+  printf("Message from client: request type: %c, number jobs: %d\n", req_type, number_jobs);
+
+  //sending
+  if (req_type == 'G'){
+    int eof_reached = 0;
+    ret = send_next_job(file, client_sock, &eof_reached);
+    if (ret == -1){
+      perror("send()");
+      return -1;
+    }
+    return 0;
+
+  } else if (req_type == 'N'){
+
+    int eof_reached = 0;
+    for (int i = 0; i < (int)number_jobs; i++){
+      ret = send_next_job(file, client_sock, &eof_reached);
+      if (ret == -1){
+        perror("send()");
+        return -1;
+      }
+      if (eof_reached){
+        break;
+      }
+    }
+    return 0;
+  } else if (req_type == 'A'){
+    int eof_reached = 0;
+    while(!eof_reached){
+      ret = send_next_job(file, client_sock, &eof_reached);
+      if (ret == -1){
+        perror("send()");
+        return -1;
+      }
+    }
+    return 0;
+  } else if (req_type == 'T'){
+    return 1;
+  } else if (req_type == 'E'){
+    return 2;
+  } else if (req_type == 'Q'){
+    return 5;
+  }
+
+  return -1;
+
+}
+
+/* Sends next job to server
+* returns return to systemcall send()
+*/
+ssize_t send_next_job(FILE* file, int client_sock, int* eof_reached){
+  char* message = read_next_job(file, eof_reached);
+  printf("Sent: work type: %c, message: %s\n", message[0], (message + 2));
+  ssize_t ret = send(client_sock, message, REPLY_MSG_SIZE, 0);
+  free(message);
+  return ret;
 }
